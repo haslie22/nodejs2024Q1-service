f9f0a5fc4706f8b6f329636d339e58c7c72df486
Lizaveta <lizadondysh@icloud.com>
2024-03-30 00:09:39 +0200

    feat: use Prisma for albums endpoint

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [AlbumService],
   controllers: [AlbumController],
-  imports: [DatabaseModule],
+  imports: [PrismaModule],
 })
 export class AlbumModule {}
