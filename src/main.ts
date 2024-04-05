55425177da354db7ef2b12d15f192d8880fc74c0
Lizaveta <lizadondysh@icloud.com>
2024-04-06 00:59:19 +0300

    feat: implement prisma exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
+import PrismaExceptionFilter from './filter/prismaException.filter';
 
 async function bootstrap() {
   const APP_PORT = process.env.APP_PORT || 4000;
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
